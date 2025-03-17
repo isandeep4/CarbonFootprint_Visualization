9fe801e732e110765cf9025ddb967258f92292e5
isandeep4 <isandeepsahoo5@gmail.com>
2025-03-17 10:24:50 -0600

    implemeted dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,89 @@
+"use client"
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import { LineChart, PieChart } from '@mui/x-charts';
+import { useEffect, useState } from "react";
+import { colors, keyToLabel, mockMonthlyData } from "../utils/mockMonthlyData";
+
+type EmissionData = {
+    "month": string,
+    "food": number,
+    "transport": number,
+    "energy": number
+}
+
+export default function Dashboard(){
+    const [emissionDataCategorically, setEmissionDataCategorically] = useState<EmissionData[]>([]);
+    useEffect(()=>{
+        const emissionData = mockMonthlyData.map((monthlyData) => ({
+            month: monthlyData.month,
+            food: monthlyData.food,
+            transport: monthlyData.transport,
+            energy: monthlyData.energy
+        }))
+        setEmissionDataCategorically(emissionData);
+    }, []);
+    return(
+        <Box>
+            <Box className="m-2">
+                <Typography variant="h4" margin={2}>
+                    Your Footprint
+                </Typography>
+                <Box sx={{
+                    margin: "2rem",
+                    display: "flex",
+                    gap: "2rem"
+                }}>
+                <Card>
+                    <Typography sx={{ padding: "1rem"}}>Total CO₂ Emissions:</Typography>
+                    <CardContent>
+                    <Typography sx={{ margin: "1rem", color: 'text.secondary', fontSize: 14 }}>
+                         1200 kg CO₂ 
+                    </Typography>
+                    </CardContent>
+                </Card>
+                {/* Emissions Over Time */}
+                <Card>
+                <Typography sx={{ padding: "1rem"}}>Emissions Over Time:</Typography>
+                    <CardContent>
+                    <PieChart
+                        series={[
+                            {
+                            data: [
+                                { id: 0, value: 10, label: 'Transport' },
+                                { id: 1, value: 15, label: 'Food' },
+                                { id: 2, value: 20, label: 'Energy' },
+                            ],
+                            },
+                        ]}
+                        width={400}
+                        height={200}
+                    />
+                    </CardContent>
+                </Card>
+                    {/* emission breakdown */}
+                <Card>
+                <Typography sx={{ padding: "1rem"}}>Emission Breakdown:</Typography>
+                    <CardContent>
+                    <LineChart
+                        xAxis={[
+                            {
+                                scaleType: "band",
+                                data: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+                            }
+                        ]}
+                        series={Object.keys(keyToLabel).map(key => ({
+                            dataKey: key,
+                            label: keyToLabel[key],
+                            color: colors[key]
+                        }))}
+                        dataset={emissionDataCategorically}
+                        height= {300}
+                        width={800}
+                    />
+                    </CardContent>
+                </Card>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
